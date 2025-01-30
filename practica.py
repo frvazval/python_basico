e8bd17351fa6c13bee3dfc333e4d21bc67d0b713
Fvazquez <foapgenerico@365.stucom.com>
2025-01-30 15:26:17 +0100

    nuevo

--- practica.py
+++ practica.py
@@ -2,6 +2,8 @@ nombre = "Francisco"
 apellido = "Vázquez"
 edad = 55
 
+# Se puede hacer de varias formas
+
 print("Me llamo " + nombre + " " + apellido + " y tengo " + str(edad) + " años")
 
 print(f"Me llamo {nombre} {apellido} y tengo {edad} años")
