3dc679d7123cc54263c849a77aab75f7d5109516
Fvazquez <foapgenerico@365.stucom.com>
2025-01-30 15:20:58 +0100

    nuevo

--- practica.py
+++ practica.py
@@ -2,4 +2,8 @@ nombre = "Francisco"
 apellido = "Vázquez"
 edad = 55
 
-print("Me llamo " + nombre + " " + apellido + "y tengo " + str(edad) + " años")
+print("Me llamo " + nombre + " " + apellido + " y tengo " + str(edad) + " años")
+
+print(f"Me llamo {nombre} {apellido} y tengo {edad} años")
+
+print("Me llamo {} {} y tengo {} años".format(nombre, apellido, edad))
