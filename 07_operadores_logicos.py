e022c2261f3d7f548d5b9e192d6365c272567113
Fvazquez <foapgenerico@365.stucom.com>
2025-01-31 15:46:17 +0100

    ultimo

---
+++ 07_operadores_logicos.py
@@ -0,0 +1,15 @@
+"""
+OPERADORES LÓGICOS
+Sirven para poder combinar diversas condiciones
+"""
+
+# and -> y
+print("True and True -> ", True and True)      # True
+print("True and False -> ", True and False)    # False
+print("False and False -> ", False and False)  # False
+
+# or -> o
+print("True or True -> ", True or True)        # True
+print("True or False -> ", True or False)      # False
+print("False or False -> ", False or False)    # False
+
