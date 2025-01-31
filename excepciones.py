383637ef73509cd9d55214e69c65b90520d8ed91
Fvazquez <foapgenerico@365.stucom.com>
2025-01-31 16:23:37 +0100

    ultimo

---
+++ excepciones.py
@@ -0,0 +1,24 @@
+"""
+EXCEPCIONES
+Son errores que se producen durante la ejecución del programa y lo interrumpen
+"""
+import os # importa libreria os
+os.system("cls") # Limpia la pantalla
+
+# try / except
+# si hay try, hay que tener except
+
+try:
+    # Intentamos ejecutar el código
+    num = float(input("Escribe un número .... "))
+    print( 1/0 )
+except ValueError:
+    print("Has de introducir un número")
+except ZeroDivisionError:
+    # Si se produce una excepción, división por cero
+    print("No se puede dividir por cero")
+except:
+    # cualquier otro error
+    print("Ha ocurrido un error")
+
+print("El programa no se interrumpe")
