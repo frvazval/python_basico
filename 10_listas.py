29620232ec2647ec305123ad487e8ce100c91943
Fvazquez <foapgenerico@365.stucom.com>
2025-02-04 16:24:45 +0100

    nuevo

--- 10_listas.py
+++ 10_listas.py
@@ -1,5 +1,5 @@
 """
-LISTAS
+lista
 """
 
 # Las listas equivalen a los "arrays" de otros lenguajes
@@ -7,20 +7,34 @@ LISTAS
 # La lista es una colección (de datos) indexada
 # el indice empieza a contar desde 0
 
-lista = [1, 2, 3, 4, 5] # lista de números enteros
+lista_numeros = [1, 2, 3, 4, 5] # lista de números enteros
 lista_nombres = ["Maria", "Pau", "Pol"] # lista de strings
 lista_de_listas = [[1, 2], [3, 4], [5, 6]] # lista de listas
 lista_mixta = [1, "Hola", True] # lista mixta
 
 # Acceder al primer valor:
 
-print(lista[0]) # el valor es 1
+print(lista_numeros[0]) # el valor es 1
 
 # acceder al ultimo valor
 
-print(lista[-1]) # el valor es 5
+print(lista_numeros[-1]) # el valor es 5
 
 # SLICING (atención, el último valor no esta incluido)
+# [inicio : final : paso]
 
-print(lista[1:3]) # [2, 3]
-print(lista[-3:-1]) # [3, 4]
+print(lista_numeros[1:3]) # [2, 3]
+print(lista_numeros[-3:-1]) # [3, 4]
+print(lista_numeros[-3:]) # [3, 4, 5]
+print(lista_numeros[::-1]) # [5, 4, 3, 2, 1]
+
+# añadir un elemento al final de la lista
+
+lista_numeros.append(6) # nombre_lista.append(valor)
+print(lista_numeros) # [1, 2, 3, 4, 5, 6]
+
+# Quitar el ultimo elemento
+
+ultimo_numero = lista_numeros.pop() # nombre_lista.pop() 
+print(lista_numeros) # [1, 2, 3, 4, 5]
+print(ultimo_numero)
