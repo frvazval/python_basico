75659beaa8d6424608b776e0feddf388a0ea6f0c
Fvazquez <foapgenerico@365.stucom.com>
2025-02-06 19:10:34 +0100

    Stucom

---
+++ Animales_que_contienen_letra.py
@@ -0,0 +1,31 @@
+# Tenemos esta lista de animales:
+# ["gato", "perro", "caballo", "paloma", "murcielago", "leon", "mono"]
+
+# Vamos a pedir una letra al usuario y mostraremos los animales que contienen esa letra.
+# Si no hay ninguno diremos "Ningún animal contiene esa letra"
+
+import os # importa libreria os        
+os.system("cls") # Limpia la pantalla
+
+animales = ["gato", "perro", "caballo", "paloma", "murcielago", "leon", "mono"]
+existe = False
+
+print("La lista de animales es: ['gato', 'perro', 'caballo', 'paloma', 'murcielago', 'leon', 'mono']")
+
+letra = input("Introduce una letra: ")
+
+# muestra los animales de la lista que contienen la letra introducida
+
+print("Los animales de la lista que contienen la letra '" + letra + "' son:")
+print()
+
+for animal in animales:
+    if letra in animal:
+        print(animal)
+        existe = True
+
+if existe == False:
+    print("Ningún animal contiene esa letra")
+
+
+
