ea7def8aaf4d5c06131f3235cf5c40a4a50bb6ce
Fvazquez <foapgenerico@365.stucom.com>
2025-02-06 16:26:46 +0100

    Stucom

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,13 @@
+# El usuario introduce un número entero, como máximo 100
+# ese número es el límite
+# Desde 0 hasta el número introducido (los dos incluidos), vamos a listar todos los números
+# Pero...
+# -- Si el número es multiplo de 3, escribiremos 3 - FIZZ
+# -- Si el número es multiplo de 5, escribiremos 5 - BUZZ
+# -- Si el número es multiplo de 3 y de 5, escribiremos 3 - FIZZ-BUZZ
+# -- En los demás casos sólo el número
+# -- Si el usuario escribe más de 100 o menos de 0, diremos "El número es incorrecto"
+
 import os # importa libreria os        
 os.system("cls") # Limpia la pantalla
 
