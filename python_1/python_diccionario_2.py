1b6baa6146de14b43596c9f1cf43aab402818635
Fvazquez <foapgenerico@365.stucom.com>
2025-02-12 19:22:42 +0100

    stucom 12/02/2025

---
+++ python_1/python_diccionario_2.py
@@ -0,0 +1,18 @@
+"""
+ENTRADAS DEL CINE
+
+Vamos a crear una app para vender entradas del cine.
+
+Hay tres precios:
+- Entrada estándar: 9.00
+- Mayores de 65 años (seniors) : 6.90
+- Infantiles : 7.20
+
+Se puede vender cualquier cantidad de entradas,
+pero los menores siempre deber ir acompañados
+de un adulto.
+
+Al finalizar la compra mostraremos las entradas 
+y el importe total. 
+
+"""
