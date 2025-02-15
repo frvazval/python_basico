84a8cff3d0f457e582546893137b51354e71a1fa
Fvazquez <fvazquez2@hotmail.com>
2025-02-15 12:42:24 +0100

    casa

--- python_1/piedra-papel-tijera-v2.py
+++ python_1/piedra-papel-tijera-v2.py
@@ -1,4 +1,4 @@
-"""
+'''
 LISTA DE MEJORAS
 
 Limitar un máximo de partidas
@@ -7,51 +7,55 @@ Preguntar el nombre del usuario
 
 Guardar los resultados
 
-"""
+'''
 
-import os # importa libreria os 
-import random # para utilizar numeros aleatorios
 
-os.system("cls") # Limpia la pantalla
 
-# lista de las opciones
+import random # Para poder utilizar números aleatorios
+import os # Para poder utilizar comandos del sistema
+os.system("cls") # Utiliza el comando del sistema cls para borrar la pantalla
+
+# Lista de las opciones
 # opciones_juego = ["Piedra", "Papel", "Tijeras"]
-opciones_juego = ["🥌", "⬜", "✂"]
+opciones_juego = ['🪨','🗒️', '✂️' ] # Lista que contiene las opciones de juego que puede elegir el jugador
 
-# variables
+# Contadores para saber cuantas partidas ganadas, perdidas o empatadas, han habido despues de jugar el numero de partidas elegido
 partidas_ganadas = 0
 partidas_perdidas = 0
-partidas_empatadas = 0
+empates = 0
 
-# Se pide el nombre al usuario
+# Pide el nombre al jugador, para poder personalizar los mensajes
 nombre_usuario = input("Escribe tu nombre --> ")
-print(f"Buena suerte {nombre_usuario}!")
+print(f"¡Buena suerte {nombre_usuario}!")
 
+# Bucle que se repetira mientras no se elija un número de partidas valido o la opción 0 para salir
 while True:
-    try:
-        numero_partidas = int(input("¿Quantas partidas quieres jugar (entre 1 y 5, 0 para salir)? --> "))
-        if numero_partidas == 0:            
-            print(f"\nHasta pronto {nombre_usuario}!")           
-            # os.system("exit")
-            break
-        elif 1 <= numero_partidas <= 5: # Si es un numero de partidas correcto
+    try: # Para controlar las excepciones, en caso de error de conversión a entero
+        numero_partidas = int(input("¿Cuántas partidas quieres jugar?\n(entre 1 y 5, 0 para salir) --> "))
+
+        # Si se elije 0 sale del juego con un mensaje de despedida
+        if numero_partidas == 0:
+            print(f"¡Hasta pronto {nombre_usuario}!")
+            # os.system("exit"), es para utilizar el comando exit para salir de la aplicación
+            # pero no es necesario y por eso esta comentado
             break # Sale del bucle
-
+        elif 1 <= numero_partidas <= 5:
+            break # Sale del bucle, con el número de partidas seleccionado
         else:
-            print("\nHas de introducir un número entre 1 y 5\n")
-
-
+            # Se muestra en el caso de que se elija una opción incorrecta
+            print("Has de introducir un número entre 1 y 5\n")
     except:
-        print("\nHas de introducir un número entero\n")
+        # Se muestra en el caso de que ocurra un error al introducir una letra 
+        # y se intente convertir a entero
+        print("Has de introducir un número entero\n")
 
-contador_de_partidas = 1 # para que entre en el bucle, si eliges 0 partidas ya no entra
+
+contador_de_partidas = 1 # Se le da el valor 1, porque sino no entraria en el bucle la primera vez
 
 while contador_de_partidas <= numero_partidas:
-    contador_de_partidas += 1 # cada vez que juega una partida incrementa en 1 el contador
-
+    contador_de_partidas += 1
     # Informar al usuario de las opciones del juego
-
-    menu =f"""
+    menu = f"""
     PIEDRA - PAPEL - TIJERAS
     ========================
 
@@ -63,42 +67,38 @@ while contador_de_partidas <= numero_partidas:
 
     """
 
-    print(menu)
-
-    # Recoger eleccion del usuario
+    print(menu) # Se muestra por pantalla el menú que contiena la variable menu
 
     opcion_humano = input("Elige tu opción --> ").strip()
 
     if opcion_humano not in ["1","2","3"]:
-        print("\nJuego finalizado. Hasta pronto!\n")
+        print("Juego finalizado. ¡Hasta pronto!")
     else:
-        opcion_pc = str(random.randint(1,3))
 
-        # variable que guarda la elección del jugador y del pc
+        opcion_maquina = str(random.randint(1,3))
+
         resultado_partida = f"""
         Has elegido {opciones_juego[int(opcion_humano)-1]}
-        La maquina ha elegido {opciones_juego[int(opcion_pc)-1]}
+        La máquina ha elegido {opciones_juego[int(opcion_maquina)-1]}
     """
+        print(resultado_partida)
 
-        # según el resultado incremento en 1 el contador correspondiente (partidad_ganadas, partidas_perdidas y partidas_empatadas)
-        if opcion_humano == opcion_pc:
-            print(resultado_partida) # muestra el resultado de la partida actual
-            print(f"\t{nombre_usuario} has empatado!!!") # \t es para que haga una tabulación
-            partidas_empatadas += 1
-        elif (opcion_humano == "1" and opcion_pc == "3") \
-            or (opcion_humano == "2" and opcion_pc == "1") \
-                or (opcion_humano == "3" and opcion_pc == "2"):
-            print(resultado_partida)
-            print(f"\t{nombre_usuario} has ganado!!!")
+        if opcion_humano == opcion_maquina:
+            empates += 1
+            print(f"{nombre_usuario}, habéis empatado")
+        elif (opcion_humano=="1" and opcion_maquina=="3") \
+            or (opcion_humano=="2" and opcion_maquina=="1") \
+                or (opcion_humano=="3" and opcion_maquina=="2"):
             partidas_ganadas += 1
+            print(f"{nombre_usuario} has ganado!!!")
         else:
-            print(resultado_partida)
-            print(f"\t{nombre_usuario} has perdido!!!")
             partidas_perdidas += 1
+            print(f"{nombre_usuario} has perdido!!!")
 
-        # en cada partida mostraremos los resultados
-        resultado_actual = f"\tGanadas: {partidas_ganadas} | Empates: {partidas_empatadas} | Perdidas: {partidas_perdidas}"
+        resultado_actual = f"""
+    Ganadas: {partidas_ganadas} | Empates : {empates} | Perdidas : {partidas_perdidas}
+        \n\n    
+""" 
         print(resultado_actual)
 
-
-print("\nAplicación finalizada\n")
+print("\nAplicación finalizada.")
