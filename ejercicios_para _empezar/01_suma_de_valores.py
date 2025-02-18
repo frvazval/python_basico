1db402de08c75373b0d0cbb6598d812894bb761b
Fvazquez <foapgenerico@365.stucom.com>
2025-02-18 15:11:59 +0100

    stucom

--- ejercicios_para _empezar/01_suma_de_valores.py
+++ ejercicios_para _empezar/01_suma_de_valores.py
@@ -4,3 +4,14 @@ Haz un programa que pida al usuario dos números para sumarlos.
 La respuesta del programa será:
 "La suma de los dos números es XX" (el valor que sea)
 """
+import os # importa libreria os        
+os.system("cls") # Limpia la pantalla
+
+try:
+    num1 = int(input("Introduce el primer número: "))
+    num2 = int(input("Introduce el segundo número: "))
+
+    print(f"La sumade  {num1} y {num2} es {num1 + num2}")
+
+except ValueError:
+    print("No has introducido un número entero valido")
