003c211f7a0a587fd5130ae2c82b2b7aad885f24
Fvazquez <foapgenerico@365.stucom.com>
2025-02-20 17:33:01 +0100

    stucom

--- poo_2/clase_coche.py
+++ poo_2/clase_coche.py
@@ -21,3 +21,22 @@ Con eso crearemos un objeto Coche, con estos valores:
 
 Ejecutar las funciones que acabas de crear.
 """
+import os # importa libreria os        
+os.system("cls") # Limpia la pantalla
+
+# Creo la clase coche
+class Coche():
+    def __init__(self, marca, color, combustible, cilindrada):
+        self.marca = marca
+        self.color = color
+        self.combustible = combustible
+        self.cilindrada = cilindrada
+    def __str__(self):
+        return f"Es de la marca {self.marca} y su color es {self.color}\n"
+    def mostrar_caracteristicas(self):
+        return f"marca: {self.marca}, color: {self.color}, combustible: {self.combustible} y cilindrada {self.cilindrada}\n"
+    
+
+
+# Creo un objeto de la clase Coche
+coche = Coche("Opel", "rojo", "electrico", 1.6)
