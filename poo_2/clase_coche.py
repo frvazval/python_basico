bd3fd778d60b726d4c8b947b3db66a8ff69fe0dc
Fvazquez <foapgenerico@365.stucom.com>
2025-02-20 17:16:14 +0100

    stucom

--- poo_2/clase_coche.py
+++ poo_2/clase_coche.py
@@ -1,3 +1,23 @@
 """
+Crear una clase llamada Coche
 
+Tendrá los siguientes atributos:
+-- marca
+-- color
+-- combustible
+-- cilindrada
+
+Crear la función __init__ con los parámetros anteriores.
+
+Crear otra función llamada "mostrar_caracteristicas" que muestre todos los detalles anteriores en un único mensaje.
+
+Crear la función __str__ que tendrá como salida la marca y el color.
+
+Con eso crearemos un objeto Coche, con estos valores:
+-- Opel
+-- rojo
+-- electrico
+-- 1.6
+
+Ejecutar las funciones que acabas de crear.
 """
