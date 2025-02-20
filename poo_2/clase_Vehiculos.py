ea8b6eb752cf4291e66c3adb743682277114e314
Fvazquez <foapgenerico@365.stucom.com>
2025-02-20 19:28:21 +0100

    stucom

---
+++ poo_2/clase_Vehiculos.py
@@ -0,0 +1,21 @@
+'''
+Crea una clase llamada Vehiculo.
+
+En su constructor incluye marca, modelo y año de construcción.
+
+Dos métodos también:
+-- arrancar, con el mensaje "El vehículo XX modelo YY ha arrancado"
+-- detener, con el mensaje "El vehículo XX modelo YY se ha detenido"
+
+Luego, dos clases hijas: Coche y Moto
+
+La clase Coche tiene dos atributos propio: numero de puertas y AC (verdadero o falso).
+y también un método propio: abrir_maletero, que
+devuelve este mensaje: "El maletero del [marca] [modelo] está abierto"
+
+La clase Moto tiene un método propio: revisar_seguridad, devuelve este mensaje:
+"Si circulas en motocicleta debes llevar casco"
+'''
+import os # importa libreria os        
+os.system("cls") # Limpia la pantalla
+
