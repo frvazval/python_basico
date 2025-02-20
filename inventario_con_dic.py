e5a2165a478a313db941703e24e090a4293d1dae
Fvazquez <foapgenerico@365.stucom.com>
2025-02-20 19:26:33 +0100

    stucom

--- inventario_con_dic.py
+++ inventario_con_dic.py
@@ -13,3 +13,41 @@ Mostrar ahora todos los productos y sus cantidades
 
 import os # importa libreria os        
 os.system("cls") # Limpia la pantalla
+
+producto =  {}
+lista_productos = []
+
+try:
+    for i in range(5):
+       num_producto = int(input("Numero de producto: "))
+       nombre = input("Nombre: ")
+       cantidad = input("Cantidad: ")
+       producto["numero"] = num_producto
+       producto["nombre"] = nombre
+       producto["cantidad"] = cantidad
+       lista_productos.append(producto)
+    print(lista_productos)
+
+    for i in range(2):
+       num_producto = int(input("Numero de producto: "))
+       nombre = input("Nombre: ")
+       cantidad = input("Cantidad: ")
+       producto["numero"] = num_producto
+       producto["nombre"] = nombre
+       producto["cantidad"] = cantidad
+       lista_productos.append(producto)
+
+    print(lista_productos)
+
+    for prod in lista_productos:
+        if prod["numero"] == 4:
+            prod["cantidad"] = 40
+        if prod["numero"] == 6:
+            prod["cantidad"] = 60        
+
+    for prod in lista_productos:
+        print(f"Num: {prod['numero']}, nombre producto: {prod['nombre']} y cantidad: {prod['cantidad']}")
+
+    
+except ValueError:
+    print("Has introducido una cantidad incorrecta")
