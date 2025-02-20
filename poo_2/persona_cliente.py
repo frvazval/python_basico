27f9ec068440b0bf70565c8baaf9b7471f3d8b17
Fvazquez <foapgenerico@365.stucom.com>
2025-02-20 17:41:26 +0100

    stucom

--- poo_2/persona_cliente.py
+++ poo_2/persona_cliente.py
@@ -1,5 +1,23 @@
 """
+Vamos a crear una clase llamada Persona.
 
+tendrá estos atributos:
+-- nombre
+-- apellido
+-- ciudad
+
+Crear el constructor y el método que muestra la información de la clase (print(objeto))
+
+También Habrá una clase hija llamada Cliente.
+
+Tendrá además los atributos:
+-- dni
+-- compras
+
+Cuando se muestre el objeto deben aparecer todos los atributos.
+
+Hay que crear un método llamado compras_realizadas que tenga esta salida:
+El cliente Fulanito ha comprado xx.xx €
 """
 
 import os # importa libreria os        
