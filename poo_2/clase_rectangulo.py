132e01289d20e9a7f2c2b2d074c9f55185f05cbe
Fvazquez <foapgenerico@365.stucom.com>
2025-02-20 18:29:43 +0100

    stucom

---
+++ poo_2/clase_rectangulo.py
@@ -0,0 +1,14 @@
+"""
+Hay que crear una clase llamada Rectangulo.
+
+Necesitamos los métodos para obtener el área, el perímetro y la diagonal de la figura.
+
+Cada uno en un método diferente.
+
+Lo probaremos con un rectangulo de lados 3 y 4
+
+"""
+
+import os # importa libreria os        
+os.system("cls") # Limpia la pantalla
+
