b4c292fa5c4c0bab18361e0ff9ed4faafe64bd86
Fvazquez <foapgenerico@365.stucom.com>
2025-02-20 19:47:02 +0100

    stucom

--- poo_2/clase_Vehiculos.py
+++ poo_2/clase_Vehiculos.py
@@ -19,3 +19,38 @@ La clase Moto tiene un método propio: revisar_seguridad, devuelve este mensaje:
 import os # importa libreria os        
 os.system("cls") # Limpia la pantalla
 
+# Creo la clase Vehiculo
+class Vehiculo():
+    def __init__(self, marca, modelo, any_const):
+        self.marca = marca
+        self.modelo = modelo
+        self.any_const = any_const
+    # Metodos
+    def arrancar(self):
+        print(f"El vehículo {self.marca} modelo {self.modelo} ha arrancado")
+    def detener(self):
+        print(f"El vehículo {self.marca} modelo {self.modelo} se ha detenido")
+# Creo las clases hijas Coche y Moto
+class Coche(Vehiculo):
+    def __init__(self, marca, modelo, any_const, num_puertas, ac):
+        super().__init__(marca, modelo, any_const)
+        self.num_puertas = num_puertas
+        self.ac = ac
+    def abrir_maletero(self):
+        print(f"El maletero del {self.marca} {self.modelo} está abierto")
+
+class Moto(Vehiculo):
+    def __init__(self, marca, modelo, any_const):
+        super().__init__(marca, modelo, any_const)
+    def revisar_seguridad(self):
+        print("Si circulas en motocicleta debes llevar casco")
+
+# Programa principal, creo objetos de ñlas clases
+
+coche_1 = Coche("SEAT", "Leon", 2000, 5, True)
+moto_1 = Moto("Honda", "SH", 2010)
+
+coche_1.arrancar()
+coche_1.detener()
+
+moto_1.revisar_seguridad()
