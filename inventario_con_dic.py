0c1f6fe88c59e83b8ad27816b6fbd9faed9b1183
Fvazquez <fvazquez2@hotmail.com>
2025-02-21 00:37:11 +0100

    casa 21/02/2025

--- inventario_con_dic.py
+++ inventario_con_dic.py
@@ -28,25 +28,25 @@ try:
        lista_productos.append(producto)
     print(lista_productos)
 
-    for i in range(2):
-       num_producto = int(input("Numero de producto: "))
-       nombre = input("Nombre: ")
-       cantidad = input("Cantidad: ")
-       producto["numero"] = num_producto
-       producto["nombre"] = nombre
-       producto["cantidad"] = cantidad
-       lista_productos.append(producto)
+    # for i in range(2):
+    #    num_producto = int(input("Numero de producto: "))
+    #    nombre = input("Nombre: ")
+    #    cantidad = input("Cantidad: ")
+    #    producto["numero"] = num_producto
+    #    producto["nombre"] = nombre
+    #    producto["cantidad"] = cantidad
+    #    lista_productos.append(producto)
 
-    print(lista_productos)
+    # print(lista_productos)
 
-    for prod in lista_productos:
-        if prod["numero"] == 4:
-            prod["cantidad"] = 40
-        if prod["numero"] == 6:
-            prod["cantidad"] = 60        
+    # for prod in lista_productos:
+    #     if prod["numero"] == 4:
+    #         prod["cantidad"] = 40
+    #     if prod["numero"] == 6:
+    #         prod["cantidad"] = 60        
 
-    for prod in lista_productos:
-        print(f"Num: {prod['numero']}, nombre producto: {prod['nombre']} y cantidad: {prod['cantidad']}")
+    # for prod in lista_productos:
+    #     print(f"Num: {prod['numero']}, nombre producto: {prod['nombre']} y cantidad: {prod['cantidad']}")
 
     
 except ValueError:
