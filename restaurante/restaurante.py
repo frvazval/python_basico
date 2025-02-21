bcaad97f9ef16154867180e9cd146b0fd508576d
Fvazquez <foapgenerico@365.stucom.com>
2025-02-21 16:30:46 +0100

    stucom

--- restaurante/restaurante.py
+++ restaurante/restaurante.py
@@ -21,3 +21,36 @@ restarurante_1 = Restaurante("Can Pizza", "Italiana", [13, 14, 15, 20, 21, 22], 
 import os # importa libreria os        
 os.system("cls") # Limpia la pantalla
 
+# Creo las clases
+class Restaurante():
+    def __init__(self, nombre, especialidad, turnos):
+        self.nombre = nombre
+        self.especialidad = especialidad
+        self.turnos = turnos
+        self.lista_clientes = []
+        self.hora = None  
+        self.reserva = {}  
+    # Metodos
+    def reservado(self):
+        print(f"Reserva realizada a ")
+    def no_reservado(self):
+        print(f"No se ha podido realizar la reserva. pruebe en otro turno")
+    def hacer_reserva(self, cliente, hora):
+        if self.clientes:
+            reserva = {"cliente": cliente, "hora": hora}
+            self.lista_clientes.append(reserva)
+        else:
+            pass
+
+class Cliente():
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+# Creo los objetos
+
+restaurante_1 = Restaurante("Can Pizza", "Italiana", (13, 14, 15, 20, 21, 22))
+
+cliente_1 = Cliente("Antonio")
+
+restaurante_1.hacer_reserva(cliente_1,13)
+
