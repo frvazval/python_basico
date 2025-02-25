d1d3734d1fb8da41edd9b753de7f1d63d1197be9
Fvazquez <foapgenerico@365.stucom.com>
2025-02-25 17:15:42 +0100

    stucom 25/02/2025

--- restaurante/restaurante_clases.py
+++ restaurante/restaurante_clases.py
@@ -27,6 +27,7 @@ class Cliente():
         self.nombre = nombre
 
 ana = Cliente("Ana")
+saludo = "hola"
 
 class Restaurante():
     '''Restaurante: nombre, especialidad, turnos'''
@@ -45,15 +46,21 @@ class Restaurante():
         if hora_reserva not in self.turnos:
             tupla_turnos = [ str(turno) for turno in self.turnos] # Guarda los turnos en una tupla en formato string para mostrarlo en el mensaje
             mensaje = f"Lo sentimos, no es posible la reserva a las {hora_reserva}\n"
-            mensaje +=  f"Horarios disponibles: " + ", ".join(tupla_turnos) + " horas" # muestra los turnos de la tupla
+            mensaje +=  f"Horarios disponibles: " + ", ".join(tupla_turnos) + " horas." # muestra los turnos de la tupla
             return mensaje
-    
-
-
+        # Comprobar las reservas anteriores
+        if self.reservas[hora_reserva] < 3:
+            self.reservas[hora_reserva] += 1
+            return  f"Reserva confirmada a las {hora_reserva} para el cliente {cliente.nombre}"
+        else:
+            return f"Lo sentimos, no es posible reservar a las {hora_reserva}"  
 
 napoli = Restaurante("Napoli", "Italiana", (12, 13, 14, 15, 20, 21, 22))
 
-print(napoli.__doc__)
-print(napoli.__dict__)
+print(napoli.__doc__) # Muestra el comentario
+print(napoli.__dict__) # Muestra el contenido
 
-print(napoli.reservar(ana, 4))
+print(napoli.reservar(ana, 14))
+print(napoli.reservar(ana, 14))
+print(napoli.reservar(ana, 14))
+print(napoli.reservar(ana, 14))
