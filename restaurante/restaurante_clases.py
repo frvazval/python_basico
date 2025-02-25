8e4560377ca4117cba4c23933789ec625462ee18
Fvazquez <foapgenerico@365.stucom.com>
2025-02-25 16:33:05 +0100

    stucom 25/02/2025

---
+++ restaurante/restaurante_clases.py
@@ -0,0 +1,59 @@
+"""
+Vamos a gestionar resturantes
+
+Cada uno tiene:
+-- nombre
+-- especialidad
+-- turnos:
+    -- Puede haber como máximo 3 clientes
+    -- si se realiza la reserva diremos "Reserva realizada a [nombre_cliente]"
+    -- y si no "No se ha podido realizar la reserva. pruebe en otro turno"
+-- clientes
+
+Del cliente vamos a necesitar (de momento)
+sólo el nombre
+
+Ejemplo de uso:
+cliente_1 = Cliente("Anna")
+restaurante_1 = Restaurante("Can Pizza", "Italiana", [13, 14, 15, 20, 21, 22])
+
+"""
+
+import os # importa libreria os        
+os.system("cls") # Limpia la pantalla
+
+class Cliente():
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+ana = Cliente("Ana")
+
+class Restaurante():
+    '''Restaurante: nombre, especialidad, turnos'''
+    def __init__(self, nombre: str, especialidad: str, turnos: tuple):
+        self.nombre = nombre
+        self.especialidad = especialidad
+        self.turnos = turnos
+        # Atributo añadido para controlar los turnos
+        self.reservas = {}
+        for turno in turnos:
+            self.reservas[turno] = 0 # pone todos los turnos a cero
+
+    # Metodo para reservar
+    def reservar(self, cliente, hora_reserva):
+        # Comprobar si la hora solicitada esta en los turnos disponibles
+        if hora_reserva not in self.turnos:
+            tupla_turnos = [ str(turno) for turno in self.turnos] # Guarda los turnos en una tupla en formato string para mostrarlo en el mensaje
+            mensaje = f"Lo sentimos, no es posible la reserva a las {hora_reserva}\n"
+            mensaje +=  f"Horarios disponibles: " + ", ".join(tupla_turnos) + " horas" # muestra los turnos de la tupla
+            return mensaje
+    
+
+
+
+napoli = Restaurante("Napoli", "Italiana", (12, 13, 14, 15, 20, 21, 22))
+
+print(napoli.__doc__)
+print(napoli.__dict__)
+
+print(napoli.reservar(ana, 4))
