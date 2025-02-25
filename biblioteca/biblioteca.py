086b91fd072e61a0499340a08df39d7f25b8fcf4
Fvazquez <foapgenerico@365.stucom.com>
2025-02-25 19:46:13 +0100

    stucom 25/02/2025

--- biblioteca/biblioteca.py
+++ biblioteca/biblioteca.py
@@ -13,7 +13,7 @@ El programa debe crear las siguientes clases con sus métodos:
     - agregar_lector: agrega un lector a la biblioteca
     - mostrar lectores
     - agregar_libro: agrega un libro a la biblioteca,
-        indicando los ejemplare disponibles
+        indicando los ejemplares disponibles
     - buscar_libro: busca un libro en la biblioteca, 
         indicando si lo tiene o no
     - mostrar libros de la biblioteca
@@ -41,7 +41,8 @@ class Biblioteca():
         self.nombre = nombre
         self.direccion = direccion
         self.lista_lectores = []
-        self.lista_libros = []        
+        self.lista_libros = [] 
+        self.cantidad_libros = {}       
 
     # Metodos de la clase Biblioteca
     def mostrar_libros(self):
@@ -54,20 +55,29 @@ class Biblioteca():
             for lector in self.lista_lectores:
                 print(f"Nombre: {lector.nombre} Apellido: {lector.apellido}\n")
 
-    def agregar_lector(self, lector):
-        
+    def agregar_lector(self, lector):        
         self.lista_lectores.append(lector)
 
     def agregar_libro(self, libro):
+        if self.lista_libros: # Si la lista no esta vacia
+            if libro not in self.lista_libros:  # Si no esta en la lista , lo añade      
+                self.lista_libros.append(libro) 
+                self.cantidad_libros[libro] == 1 # Y pone la cantidad a 1
+            else:
+                for libro_actual in self.lista_libros:
+                    if libro_actual == libro:
+                        self.cantidad_libros[libro_actual] += 1 # Si ya existe, le añado 1 a la cantidad
+        else: # Si la lista esta vacia
+            self.lista_libros.append(libro) 
+            self.cantidad_libros[libro] == 1 # Y pone la cantidad a 1
         
-        self.lista_libros.append(libro)
 
     def buscar_libro(self, libro_buscado):
-        valor = "El libro que buscas no existe en esta biblioteca"
+        valor = f"El libro '{libro_buscado.titulo}' no existe en esta biblioteca\n"
         if self.lista_libros:
             for libro in self.lista_libros:
-                if libro_buscado == libro and libro_buscado == libro and libro_buscado == libro:
-                    valor = "El libro si que existe en la biblioteca"        
+                if libro_buscado == libro:
+                    valor = f"El libro '{libro_buscado.titulo}' si que existe en la biblioteca, \n"        
         return valor
         
 
@@ -85,6 +95,7 @@ biblioteca_1 = Biblioteca("Biblioteca municipal", "Av. Masnou")
 # Agrego los libros a la biblioteca
 biblioteca_1.agregar_libro(libro_1)
 biblioteca_1.agregar_libro(libro_2)
+biblioteca_1.agregar_libro(libro_2)
 
 # Agrego los lectores a la biblioteca
 biblioteca_1.agregar_lector(lector_1)
@@ -97,4 +108,7 @@ biblioteca_1.mostrar_lectores()
 biblioteca_1.mostrar_libros()
 
 print(biblioteca_1.buscar_libro(libro_1))
+print(biblioteca_1.buscar_libro(libro_2))
 print(biblioteca_1.buscar_libro(libro_3))
+
+
