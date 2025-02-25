109020bd6d1d4f3ef2208030bf69b3d4f87fa862
Fvazquez <foapgenerico@365.stucom.com>
2025-02-25 17:17:47 +0100

    stucom 25/02/2025

---
+++ biblioteca/biblioteca.py
@@ -0,0 +1,26 @@
+"""
+Programación Orientada a Objetos: Biblioteca
+
+El programa debe crear las siguientes clases con sus métodos:
+
+    Clase Lector, que se construirá con nombre y apellido
+
+    Clase Libro, que se construirá con nombre_autor, apellido_autor,
+    y título
+
+    Clase Biblioteca, que se construirá con nombre y dirección
+    Esta clase dispondrá de los siguientes métodos:
+    - agregar_lector: agrega un lector a la biblioteca
+    - mostrar lectores
+    - agregar_libro: agrega un libro a la biblioteca,
+        indicando los ejemplare disponibles
+    - buscar_libro: busca un libro en la biblioteca, 
+        indicando si lo tiene o no
+    - mostrar libros de la biblioteca
+
+
+"""
+
+import os # importa libreria os        
+os.system("cls") # Limpia la pantalla
+
