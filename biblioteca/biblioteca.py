d37c6b52834328c5267f38413602b6581227df50
Fvazquez <foapgenerico@365.stucom.com>
2025-02-25 18:03:52 +0100

    stucom 25/02/2025

--- biblioteca/biblioteca.py
+++ biblioteca/biblioteca.py
@@ -24,3 +24,70 @@ El programa debe crear las siguientes clases con sus métodos:
 import os # importa libreria os        
 os.system("cls") # Limpia la pantalla
 
+# Creo las clases
+class Lector():
+    def __init__(self, nombre: str, apellido: str):
+        self.nombre = nombre
+        self.apellido = apellido
+
+class Libro():
+    def __init__(self, nombre_autor: str, apellido_autor: str, titulo: str):
+        self.nombre_autor = nombre_autor
+        self.apellido_autor = apellido_autor
+        self.titulo = titulo
+
+class Biblioteca():
+    def __init__(self, nombre: str, direccion: str):
+        self.nombre = nombre
+        self.direccion = direccion
+        self.lista_lectores = []
+        self.lista_libros = [] 
+        self.libro = {}
+        self.lector = {}      
+
+    # Metodos de la clase Biblioteca
+    def mostrar_libros():
+        if self.lista_libros:
+            for libro in self.lista_libros:
+                print(f"Titulo: {libro['titulo']}, Autor: {libro['nombre']} {libro['apellido']}")
+        
+    def mostrar_lectores():
+        if self.lista_lectores:
+            for lector in self.lista_lectores:
+                print(f"Nombre: {lector['nombre']} Apellido: {lector['apellido']} ")
+
+    def agregar_lector(self, lector):
+        self.lector = {"nombre": lector.nombre, "apellido": lector.apellido}
+        self.lista_lectores.append(self.lector)
+
+    def agregar_libro(self, libro):
+        self.libro = {"nombre": libro.nombre_autor, "apellido": libro.apellido_autor, "titulo": libro.titulo}
+        self.lista_libros.append(self.libro)
+
+    def buscar_libro(self, libro):
+        pass
+
+
+# Creo los ojetos -> lector, libro y biblioteca
+lector_1 = Lector("Antonio", "Lopez")
+lector_2 = Lector("Maria", "Perez")
+
+libro_1 = Libro("Carlos", "Zafón", "La sombra del viento")
+libro_2 = Libro("Dolores", "Redondo", "Ofrenda a la tormenta")
+
+biblioteca_1 = Biblioteca("Biblioteca municipal", "Av. Masnou")
+
+# Agrego los libros a la biblioteca
+biblioteca_1.agregar_libro(libro_1)
+biblioteca_1.agregar_libro(libro_2)
+
+# Agrego los lectores a la biblioteca
+biblioteca_1.agregar_lector(lector_1)
+biblioteca_1.agregar_lector(lector_2)
+
+# Utilizo los metodos del objeto biblioteca
+
+biblioteca_1.mostrar_lectores()
+
+biblioteca_1.mostrar_libros()
+
