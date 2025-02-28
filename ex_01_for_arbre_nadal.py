0715c467a506b09e6cca612976a1ee848a759884
Fvazquez <foapgenerico@365.stucom.com>
2025-02-28 18:31:13 +0100

    stucom 28/02/2025

---
+++ ex_01_for_arbre_nadal.py
@@ -0,0 +1,28 @@
+import os # importa libreria os        
+os.system("cls") # Limpia la pantalla
+
+def dibuja_arbol(niveles):
+    linea = 20
+    num_asteriscos = 1
+
+    for nivel in range(niveles):
+        mensaje = " " * (niveles - nivel) + "*"
+        
+           
+        print(mensaje)
+        
+        
+
+
+try:
+    continuar = True
+    while continuar:
+        niveles = int(input("¿Cuantos niveles tendra el arbol de Navidad? (entre 5 y 10) -> "))
+        if 5 <= niveles <= 10:
+            continuar = False
+        else: 
+            print("Se tiene que elegir un valor entre 5 y 10\n")
+    dibuja_arbol(niveles)
+
+except ValueError:
+    print("Se tiene que introducir un número, no se pueden poner letras\n")
