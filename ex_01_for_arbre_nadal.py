9d4c6006c1d4720dbe8c2f87f592c5a8cd822e8d
Fvazquez <fvazquez2@hotmail.com>
2025-03-03 20:20:08 +0100

    casa 3/03/2025

--- ex_01_for_arbre_nadal.py
+++ ex_01_for_arbre_nadal.py
@@ -3,11 +3,16 @@ os.system("cls") # Limpia la pantalla
 
 def dibuja_arbol(niveles):
     linea = 20
-    num_asteriscos = 1
+    num_asteriscos = 1 # Asteriscos de la primaera linea
 
     for nivel in range(niveles):
-        mensaje = " " * (niveles - nivel) + "*"
-        
+        mensaje = " " * (niveles - nivel)
+
+        if nivel != 0: # Si no es la primera linea
+            num_asteriscos = (nivel * 2) + 1
+            mensaje += "*" * num_asteriscos
+        else: # Si es la primera linea
+            mensaje += "*"        
            
         print(mensaje)       
 
